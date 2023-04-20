32db6fd2cd86649144077a41001e58c4972c9f87
Tran Do Hoa <trandohoa93@gmail.com>
2023-04-20 14:43:55 +0700

    account page

--- src/Components/DropdownLogin/DropdownLogin.tsx
+++ src/Components/DropdownLogin/DropdownLogin.tsx
@@ -1,10 +1,12 @@
 import classNames from 'classnames/bind';
+import { useDispatch } from 'react-redux';
 
 import IconCancel from '../../Assets/Images/icon-cancel.svg';
 import IconLogout from '../../Assets/Images/Icon-logout.svg';
 import IconMallBag from '../../Assets/Images/icon-mallbag.svg';
 import IconReview from '../../Assets/Images/Icon-Reviews.svg';
 import IconUser from '../../Assets/Images/user (1).svg';
+import { logout } from '../../Features/User/userLogin';
 import styles from './DropdownLogin.module.scss';
 
 type Props = {
@@ -13,6 +15,12 @@ type Props = {
 
 const cx = classNames.bind(styles);
 function DropdownLogin({ handleDropdown }: Props) {
+  const dispatch = useDispatch();
+
+  const handleLogout = () => {
+    dispatch(logout());
+  };
+
   return (
     <ul className={cx('inner', { show: !handleDropdown })}>
       <li>
@@ -31,10 +39,12 @@ function DropdownLogin({ handleDropdown }: Props) {
         <img src={IconReview} alt="IconReview" />
         My reviews
       </li>
-      <li>
-        <img src={IconLogout} alt="IconLogout" />
-        Logout
-      </li>
+      <button onClick={() => handleLogout()}>
+        <li>
+          <img src={IconLogout} alt="IconLogout" />
+          Logout
+        </li>
+      </button>
     </ul>
   );
 }
--- src/Pages/Account/Account.tsx
+++ src/Pages/Account/Account.tsx
@@ -1,7 +1,106 @@
+import classNames from 'classnames/bind';
+
+import Button from '../../Components/Button';
 import styles from './Account.module.scss';
 
+const cx = classNames.bind(styles);
 function Account() {
-  return <div>Account</div>;
+  return (
+    <div className={cx('wrapper')}>
+      <div className={cx('inner')}>
+        <div className={cx('roadtext')}>
+          <div>
+            <span className={cx('main')}>
+              Home <span>/ </span>
+            </span>
+            <span className={cx('sub')}>Contact</span>
+          </div>
+          <div>
+            <p>
+              Welcome <span className={cx('name')}>Md Rimel</span>
+            </p>
+          </div>
+        </div>
+        <div className={cx('content')}>
+          <div className={cx('user-dropdown')}>
+            <div className={cx('item')}>
+              Manage My Account
+              <ul>
+                <li>My Profile</li>
+                <li>Address Book</li>
+                <li>My Payment Options</li>
+              </ul>
+            </div>
+            <div className={cx('item')}>
+              My Orders
+              <ul>
+                <li>My Returns</li>
+                <li>My Cancellations</li>
+              </ul>
+            </div>
+            <div className={cx('item')}>My WishList</div>
+          </div>
+          <div className={cx('info')}>
+            <form>
+              <p className={cx('title')}> Edit Your Profile</p>
+              <div className={cx('group-input')}>
+                <div className={cx('name')}>
+                  <div>
+                    <p className={cx('text-input')}>First Name</p>
+                    <input defaultValue={'MD'} />
+                  </div>
+                  <div>
+                    <p className={cx('text-input')}>Last Name</p>
+                    <input defaultValue={'Rimel'} />
+                  </div>
+                </div>
+                <div className={cx('contact')}>
+                  <div>
+                    <p className={cx('text-input')}>Email</p>
+                    <input defaultValue={'rimel11111@gmail.com'} />
+                  </div>
+                  <div>
+                    <p className={cx('text-input')}>Address</p>
+                    <input defaultValue={'Kingston, 5236, United State'} />
+                  </div>
+                </div>
+                <div className={cx('password')}>
+                  <p className={cx('text-input')}>Password Changes</p>
+                  <div className={cx('input-password')}>
+                    <input defaultValue={'Current Password'}></input>
+                    <input defaultValue={'New Password'}></input>
+                    <input defaultValue={'Confirm New Password'}></input>
+                  </div>
+                </div>
+                <div className={cx('button')}>
+                  <div>
+                    <Button
+                      color="secondary"
+                      onClick={() => {
+                        console.log('hhehe');
+                      }}
+                    >
+                      Cancel
+                    </Button>
+                  </div>
+                  <div>
+                    <Button
+                      color="secondary"
+                      onClick={() => {
+                        console.log('hhehe');
+                      }}
+                    >
+                      Save Changes
+                    </Button>
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Account;
